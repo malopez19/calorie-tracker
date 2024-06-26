2248d9fa314d87f44ce217c0d8f5fcae3b18fa72
mjuradol <lopezmateojurado@gmail.com>
2024-06-25 19:26:32 -0500

    add actions in reducer, reinit form and unics id's

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,23 @@
-import { useState, ChangeEvent, FormEvent } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { v4 as uuidv4 } from 'uuid'
 import { categories } from "../data/categories"
 import { Activity } from "../types"
+import { ActivityActions } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
 
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: '',
-    calories: 0
-  })
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: '',
+  calories: 0
+}
+
+export default function Form({dispatch} : FormProps) {
+
+  const [activity, setActivity] = useState<Activity>(initialState)
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category', 'calories'].includes(e.target.id)
@@ -27,7 +36,12 @@ export default function Form() {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('Submit')
+
+    dispatch({type: 'SAVE_ACTIVITY', payload: {newActivity: activity}})
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
 
   return (
---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,25 @@
+import { Activity } from "../types";
+
+export type ActivityActions = {
+  type: 'SAVE_ACTIVITY', payload: { newActivity : Activity}
+}
+
+type ActivityState={
+  activities: Activity[];
+}
+
+export const initialState : ActivityState = {
+  activities: []
+}
+
+export const activityReducer = (state : ActivityState = initialState, action : ActivityActions) => {
+  if(action.type === 'SAVE_ACTIVITY'){
+
+    return {
+      ...state,
+      activities: [...state.activities, action.payload.newActivity]
+    }
+  }
+  
+  return state
+}
